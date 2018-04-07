848bd9c1ab06ffaec0d07fcd6b04b66fd95f8d4c
xLanceloth <edencarlo2431@gmail.com>
2018-04-07 15:57:23 +0800

    modified files

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/*
 function aDropdown() {
     document.getElementById("dropdown-options").classList.toggle("show");
 }
@@ -14,4 +15,18 @@ window.onclick = function (event) {
             }
         }
     }
+}
+*/
+
+function display(target) {
+    var x = document.getElementById(target);
+    var y = document.getElementById("bttndisp");
+    
+    if (x.style.display == "block") {
+        x.style.display = "none";
+        y.style.
+    } else {
+        x.style.display = "block";
+    }
+    
 }
