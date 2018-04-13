87c6b70909210a9d7c05aa4a2c7bbc8bc7ebd7ba
joshbantayan <joshuabantayan31@gmail.com>
2018-04-13 14:51:40 +0800

    styling and .json

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,18 @@
-/*
-function aDropdown() {
-    document.getElementById("dropdown-options").classList.toggle("show");
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbutton')) {
-        
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-*/
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-function display(target) {
-    var x = document.getElementById(target);
-    var y = document.getElementById("bttndisp");
-    
-    if (x.style.display == "block") {
-        x.style.display = "none";
-        y.style.
-    } else {
-        x.style.display = "block";
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-    
+  }
 }
